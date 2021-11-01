bc47073c9e5d12a4dcc97ffe67d80fc2f553e599
boy52hz <ggwp.zero@gmail.com>
2021-11-01 21:17:44 +0700

    added: dom assignment folder

--- nike-shoes-store/products.js
+++ nike-shoes-store/products.js
@@ -1,14 +1,58 @@
 export const products = [
     {
-        id: 'NS01',
-        name: 'Nike Air Jordan 1 Mid SE',
-        desc: 'Air Jordan 1 Mid SE ยังคงเสน่ห์เหนือกาลเวลาของ AJ1 รุ่นแรกเอาไว้ พร้อมปรับโฉมด้วยสีสันสดใหม่เพื่อให้รองเท้ามีรูปโฉมใหม่ คู่นี้มีส่วน Air-Sole น้ำหนักเบาพร้อมวัสดุผสมจากธรรมชาติและวัสดุสังเคราะห์ช่วยตกแต่งให้กับลุคคลาสสิก',
-        price: 4600
+        id: 'P001',
+        name: 'Jordan One Take II PF',
+        type: 'รองเท้าบาสเก็ตบอล',
+        price: 2879,
+        qtyInStock : 20
     },
     {
-        id: 'NS022',
-        name: 'Nike Air Force 1 \'07',
-        desc: 'ความเบิกบานดำรงอยู่ใน Nike Air Force 1 ’07 รองเท้าบาสรุ่นไอคอนที่ใส่ลูกเล่นสดใหม่ให้กับสิ่งที่คุณรู้จักดีที่สุด ไม่ว่าจะเป็นหนังเฉียบคม สีสันสะดุดตา และความวิบวับที่ลงตัวซึ่งทำให้คุณเปล่งประกาย',
-        price: 3500
+        id:'P002',
+        name: 'Air Jordan 1 Hi FlyEase',
+        type: 'รองเท้าเด็กโต',
+        price: 3400,
+        qtyInStock : 76
+    },
+    {
+        id: 'P003',
+        name: 'Jordan 6 Retro Little Flex',
+        type: 'รองเท้าเด็กเล็ก',
+        price: 2400,
+        qtyInStock : 442
+    },
+    {
+        id: 'P004',
+        name: 'Air Jordan 6 Retro',
+        type: 'รองเท้า',
+        price: 6700,
+        qtyInStock : 23
+    },
+    {
+        id: 'P005',
+        name: 'Air Jordan 1 Mid SE',
+        type: 'รองเท้า',
+        price: 3400,
+        qtyInStock : 82
+    },
+    {
+        id: 'P006',
+        name: 'Air Jordan Retro 12',
+        type: 'รองเท้า',
+        price: 6700,
+        qtyInStock : 43
+    },
+    {
+        id: 'P007',
+        name: 'Jordan Break',
+        type: 'รองเท้าแตะแบบสวม',
+        price: 1100,
+        qtyInStock : 121
+    },
+    {
+        id: 'P008',
+        name: 'Air Jordan XXXV',
+        type: 'รองเท้าบาสเก็ตบอล',
+        price: 4879,
+        qtyInStock : 32
     }
 ];
